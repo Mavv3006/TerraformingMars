a3497ca8417bd756d1a43aa49525d5708e117aa0
Mavv <marvdeus@gmail.com>
2020-02-27 18:06:41 +0100

    Energy: added History logging

--- lib/models/terraformingValueData/energy.dart
+++ lib/models/terraformingValueData/energy.dart
@@ -1,3 +1,5 @@
+import 'package:terraforming_mars/models/history/historyMessage.dart';
+import 'package:terraforming_mars/models/history/historyMessageType.dart';
 import 'package:terraforming_mars/models/terraformingValueData/ressourceValue.dart';
 
 class Energy extends RessourceValue {
@@ -10,7 +12,72 @@ class Energy extends RessourceValue {
     _oldValue = value;
     value = production;
     notifyListeners();
+    history.log(
+      HistoryMessage(
+        message: "Energy - next Round",
+        oldValue: _oldValue,
+        newValue: value,
+        type: Energy,
+        historyMessageType: HistoryMessageType.NEXT_ROUND,
+      ),
+    );
   }
 
   int get oldValue => _oldValue;
+
+  @override
+  void decrementValue() {
+    super.decrementValue();
+    history.log(
+      HistoryMessage(
+        message: "Energy - decrement Value",
+        oldValue: value + 1,
+        newValue: value,
+        type: Energy,
+        historyMessageType: HistoryMessageType.VALUE,
+      ),
+    );
+  }
+
+  @override
+  void incrementValue() {
+    super.incrementValue();
+    history.log(
+      HistoryMessage(
+        message: "Energy - increment Value",
+        oldValue: value - 1,
+        newValue: value,
+        type: Energy,
+        historyMessageType: HistoryMessageType.VALUE,
+      ),
+    );
+  }
+
+  @override
+  void decrementProduction() {
+    super.decrementProduction();
+    history.log(
+      HistoryMessage(
+        message: "Energy - decrement Production",
+        oldValue: production + 1,
+        newValue: production,
+        type: Energy,
+        historyMessageType: HistoryMessageType.PRODUCTION,
+      ),
+    );
+  }
+
+  @override
+  void incrementProduction() {
+    super.incrementProduction();
+    history.log(
+      HistoryMessage(
+        message: "Energy - increment Production",
+        oldValue: production - 1,
+        newValue: production,
+        type: Energy,
+        historyMessageType: HistoryMessageType.PRODUCTION,
+      ),
+    );
+  }
 }
