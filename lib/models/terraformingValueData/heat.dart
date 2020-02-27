2dd895ff040a8b22e5a3ef31a3c81e293de89533
Mavv <marvdeus@gmail.com>
2020-02-27 18:05:29 +0100

    Heat: added History logging

--- lib/models/terraformingValueData/heat.dart
+++ lib/models/terraformingValueData/heat.dart
@@ -1,3 +1,5 @@
+import 'package:terraforming_mars/models/history/historyMessage.dart';
+import 'package:terraforming_mars/models/history/historyMessageType.dart';
 import 'package:terraforming_mars/models/terraformingValueData/ressourceValue.dart';
 
 import 'energy.dart';
@@ -11,10 +13,75 @@ class Heat extends RessourceValue {
   void nextRound() {
     value += (energy.oldValue + production);
     notifyListeners();
+    history.log(
+      HistoryMessage(
+        message: "Heat - next Round",
+        oldValue: value - (energy.oldValue + production),
+        newValue: value,
+        type: Heat,
+        historyMessageType: HistoryMessageType.NEXT_ROUND,
+      ),
+    );
   }
 
   Heat updateEnergy(Energy energy) {
     this.energy = energy;
     return this;
+  }
+
+  @override
+  void decrementValue() {
+    super.decrementValue();
+    history.log(
+      HistoryMessage(
+        message: "Heat - decrement Value",
+        oldValue: value + 1,
+        newValue: value,
+        type: Heat,
+        historyMessageType: HistoryMessageType.VALUE,
+      ),
+    );
+  }
+
+  @override
+  void incrementValue() {
+    super.incrementValue();
+    history.log(
+      HistoryMessage(
+        message: "Heat - increment Value",
+        oldValue: value - 1,
+        newValue: value,
+        type: Heat,
+        historyMessageType: HistoryMessageType.VALUE,
+      ),
+    );
+  }
+
+  @override
+  void decrementProduction() {
+    super.decrementProduction();
+    history.log(
+      HistoryMessage(
+        message: "Heat - decrement Production",
+        oldValue: production + 1,
+        newValue: production,
+        type: Heat,
+        historyMessageType: HistoryMessageType.PRODUCTION,
+      ),
+    );
+  }
+
+  @override
+  void incrementProduction() {
+    super.incrementProduction();
+    history.log(
+      HistoryMessage(
+        message: "Heat - increment Production",
+        oldValue: production - 1,
+        newValue: production,
+        type: Heat,
+        historyMessageType: HistoryMessageType.PRODUCTION,
+      ),
+    );
   }
 }
